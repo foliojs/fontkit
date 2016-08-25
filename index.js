c3823e2890849f9b01303f01e47a7418bd84adbe
Devon Govett <devongovett@gmail.com>
2016-08-25 15:49:07 -0400

    Update to babel 6

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import fontkit from './base';
+import TTFFont from './src/TTFFont';
+import WOFFFont from './src/WOFFFont';
+import WOFF2Font from './src/WOFF2Font';
+import TrueTypeCollection from './src/TrueTypeCollection';
+import DFont from './src/DFont';
 
 // Register font formats
-fontkit.registerFormat(require('./src/TTFFont'));
-fontkit.registerFormat(require('./src/WOFFFont'));
-fontkit.registerFormat(require('./src/WOFF2Font'));
-fontkit.registerFormat(require('./src/TrueTypeCollection'));
-fontkit.registerFormat(require('./src/DFont'));
+fontkit.registerFormat(TTFFont);
+fontkit.registerFormat(WOFFFont);
+fontkit.registerFormat(WOFF2Font);
+fontkit.registerFormat(TrueTypeCollection);
+fontkit.registerFormat(DFont);
 
 export default fontkit;
--- src/tables/index.js
+++ src/tables/index.js
@@ -2,74 +2,74 @@ let tables = {};
 export default tables;
 
 // Required Tables
-tables.cmap = require('./cmap');
-tables.head = require('./head');
-tables.hhea = require('./hhea');
-tables.hmtx = require('./hmtx');
-tables.maxp = require('./maxp');
-tables.name = require('./name');
-tables['OS/2'] = require('./OS2');
-tables.post = require('./post');
+tables.cmap = require('./cmap').default;
+tables.head = require('./head').default;
+tables.hhea = require('./hhea').default;
+tables.hmtx = require('./hmtx').default;
+tables.maxp = require('./maxp').default;
+tables.name = require('./name').default;
+tables['OS/2'] = require('./OS2').default;
+tables.post = require('./post').default;
 
 // TrueType Outlines
-tables['cvt '] = require('./cvt');
-tables.fpgm = require('./fpgm');
-tables.loca = require('./loca');
-tables.prep = require('./prep');
-tables.glyf = require('./glyf');
+tables['cvt '] = require('./cvt').default;
+tables.fpgm = require('./fpgm').default;
+tables.loca = require('./loca').default;
+tables.prep = require('./prep').default;
+tables.glyf = require('./glyf').default;
 
 // PostScript Outlines
-tables['CFF '] = require('../cff/CFFFont');
-tables.VORG = require('./VORG');
+tables['CFF '] = require('../cff/CFFFont').default;
+tables.VORG = require('./VORG').default;
 
 // Bitmap Glyphs
 // tables.EBDT = require './EBDT'
 // tables.CBDT = tables.EBDT
-tables.EBLC = require('./EBLC');
+tables.EBLC = require('./EBLC').default;
 tables.CBLC = tables.EBLC;
 //tables.EBSC = require './EBSC'
-tables.sbix = require('./sbix');
+tables.sbix = require('./sbix').default;
 
-tables.COLR = require('./COLR');
-tables.CPAL = require('./CPAL');
+tables.COLR = require('./COLR').default;
+tables.CPAL = require('./CPAL').default;
 
 // Advanced OpenType Tables
-tables.BASE = require('./BASE');
-tables.GDEF = require('./GDEF');
+tables.BASE = require('./BASE').default;
+tables.GDEF = require('./GDEF').default;
 tables.GPOS = require('./GPOS').default;
-tables.GSUB = require('./GSUB');
-tables.JSTF = require('./JSTF');
+tables.GSUB = require('./GSUB').default;
+tables.JSTF = require('./JSTF').default;
 
 // Other OpenType Tables
-tables.DSIG = require('./DSIG');
-tables.gasp = require('./gasp');
-tables.hdmx = require('./hdmx');
-tables.kern = require('./kern');
-tables.LTSH = require('./LTSH');
-tables.PCLT = require('./PCLT');
-tables.VDMX = require('./VDMX');
-tables.vhea = require('./vhea');
-tables.vmtx = require('./vmtx');
+tables.DSIG = require('./DSIG').default;
+tables.gasp = require('./gasp').default;
+tables.hdmx = require('./hdmx').default;
+tables.kern = require('./kern').default;
+tables.LTSH = require('./LTSH').default;
+tables.PCLT = require('./PCLT').default;
+tables.VDMX = require('./VDMX').default;
+tables.vhea = require('./vhea').default;
+tables.vmtx = require('./vmtx').default;
 
 // Apple Advanced Typography Tables
 //tables.acnt = require './acnt'
-tables.avar = require('./avar');
+tables.avar = require('./avar').default;
 //tables.bdat = require './bdat'
 //tables.bhed = require './bhed'
 //tables.bloc = require './bloc'
-tables.bsln = require('./bsln');
+tables.bsln = require('./bsln').default;
 //tables.cvar = require './cvar'
 //tables.fdsc = require './fdsc'
-tables.feat = require('./feat');
+tables.feat = require('./feat').default;
 //tables.fmtx = require './fmtx'
-tables.fvar = require('./fvar');
+tables.fvar = require('./fvar').default;
 //tables.hsty = require './hsty'
-tables.gvar = require('./gvar');
-tables.just = require('./just');
+tables.gvar = require('./gvar').default;
+tables.just = require('./just').default;
 //tables.lcar = require './lcar'
 //tables.mort = require './mort'
-tables.morx = require('./morx');
+tables.morx = require('./morx').default;
 //tables.prop = require './prop'
 //tables.trak = require './trak'
-tables.opbd = require('./opbd');
+tables.opbd = require('./opbd').default;
 //tables.Zapf = require './Zapf'
