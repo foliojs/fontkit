2e2100719f4ef55eeae81ef7f85baf94d7eaae3c
Devon Govett <devongovett@gmail.com>
2016-09-03 18:58:11 -0700

    Fix gvar offset decoding in short format
    Only multiply decoded value by 2, not entire pointer.

--- src/tables/gvar.js
+++ src/tables/gvar.js
@@ -1,6 +1,16 @@
 import r from 'restructure';
 
 let shortFrac = new r.Fixed(16, 'BE', 14);
+class Offset {
+  static decode(stream, parent) {
+    // In short format, offsets are multiplied by 2.
+    // This doesn't seem to be documented by Apple, but it
+    // is implemented this way in Freetype.
+    return parent.flags 
+      ? stream.readUInt32BE()
+      : stream.readUInt16BE() * 2;
+  }
+}
 
 let gvar = new r.Struct({
   version: r.uint16,
@@ -10,24 +20,8 @@ let gvar = new r.Struct({
   globalCoords: new r.Pointer(r.uint32, new r.Array(new r.Array(shortFrac, 'axisCount'), 'globalCoordCount')),
   glyphCount: r.uint16,
   flags: r.uint16,
-  offsetToData: r.uint32
+  offsetToData: r.uint32,
+  offsets: new r.Array(new r.Pointer(Offset, 'void', { relativeTo: 'offsetToData', allowNull: false }), t => t.glyphCount + 1)
 });
-
-gvar.process = function(stream) {
-  let type = this.flags === 1 ? r.uint32 : r.uint16;
-  let ptr = new r.Pointer(type, 'void', { relativeTo: 'offsetToData', allowNull: false });
-  this.offsets = new r.Array(ptr, this.glyphCount + 1).decode(stream, this);
-
-  if (this.flags === 0) {
-    // In short format, offsets are multiplied by 2.
-    // This doesn't seem to be documented by Apple, but it
-    // is implemented this way in Freetype.
-    for (let i = 0; i < this.offsets.length; i++) {
-      this.offsets[i] *= 2;
-    }
-  }
-
-  return;
-};
 
 export default gvar;
