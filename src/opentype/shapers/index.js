4430d264ce1a64eafbc39b74b2e301fe0a0c8ecb
Devon Govett <devongovett@gmail.com>
2016-10-10 22:40:52 -0700

    Add more scripts to universal shaper

--- src/opentype/shapers/index.js
+++ src/opentype/shapers/index.js
@@ -15,7 +15,51 @@ const SHAPERS = {
 
   hang: HangulShaper,    // Hangul
 
-  bali: UniversalShaper,
+  bali: UniversalShaper, // Balinese
+  batk: UniversalShaper, // Batak
+  brah: UniversalShaper, // Brahmi
+  bugi: UniversalShaper, // Buginese
+  buhd: UniversalShaper, // Buhid
+  cakm: UniversalShaper, // Chakma
+  cham: UniversalShaper, // Cham
+  dupl: UniversalShaper, // Duployan
+  egyp: UniversalShaper, // Egyptian Hieroglyphs
+  gran: UniversalShaper, // Grantha
+  hano: UniversalShaper, // Hanunoo
+  java: UniversalShaper, // Javanese
+  kthi: UniversalShaper, // Kaithi
+  kali: UniversalShaper, // Kayah Li
+  khar: UniversalShaper, // Kharoshthi
+  khoj: UniversalShaper, // Khojki
+  sind: UniversalShaper, // Khudawadi
+  lepc: UniversalShaper, // Lepcha
+  limb: UniversalShaper, // Limbu
+  mahj: UniversalShaper, // Mahajani
+  // mand: UniversalShaper, // Mandaic
+  // mani: UniversalShaper, // Manichaean
+  mtei: UniversalShaper, // Meitei Mayek
+  modi: UniversalShaper, // Modi
+  // mong: UniversalShaper, // Mongolian
+  // 'nko ': UniversalShaper, // N’Ko
+  hmng: UniversalShaper, // Pahawh Hmong
+  // phag: UniversalShaper, // Phags-pa
+  // phlp: UniversalShaper, // Psalter Pahlavi
+  rjng: UniversalShaper, // Rejang
+  saur: UniversalShaper, // Saurashtra
+  shrd: UniversalShaper, // Sharada
+  sidd: UniversalShaper, // Siddham
+  sinh: UniversalShaper, // Sinhala
+  sund: UniversalShaper, // Sundanese
+  sylo: UniversalShaper, // Syloti Nagri
+  tglg: UniversalShaper, // Tagalog
+  tagb: UniversalShaper, // Tagbanwa
+  tale: UniversalShaper, // Tai Le
+  lana: UniversalShaper, // *Tai Tham
+  tavt: UniversalShaper, // Tai Viet
+  takr: UniversalShaper, // Takri
+  tibt: UniversalShaper, // Tibetan
+  tfng: UniversalShaper, // Tifinagh
+  tirh: UniversalShaper, // Tirhuta
 
   latn: DefaultShaper,   // Latin
   DFLT: DefaultShaper   // Default
