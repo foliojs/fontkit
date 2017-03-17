a0b88b7b2902477c58a9958366d4d838697827e3
Tracy Spiva <tracy@spiva.com>
2017-03-17 10:36:24 -0600

    fixed bug when no postscriptName was defined in the font

--- src/TTFFont.js
+++ src/TTFFont.js
@@ -90,6 +90,9 @@ export default class TTFFont {
    * @type {string}
    */
   get postscriptName() {
+    if (this.name === undefined || this.name.records.postscriptName === undefined) {
+        return "";
+    }
     let name = this.name.records.postscriptName;
     let lang = Object.keys(name)[0];
     return name[lang];
