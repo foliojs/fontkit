5dd5601043bdf3b8a0be7344fcb6337ca39d590c
Devon Govett <devongovett@gmail.com>
2017-05-05 21:54:37 -0700

    Move common OpenType variations structures into a separate file

--- src/tables/HVAR.js
+++ src/tables/HVAR.js
@@ -1,40 +1,6 @@
 import r from 'restructure';
 import {resolveLength} from 'restructure/src/utils';
-
-
-let F2DOT14 = new r.Fixed(16, 'BE', 14);
-let RegionAxisCoordinates = new r.Struct({
-  startCoord: F2DOT14,
-  peakCoord: F2DOT14,
-  endCoord: F2DOT14
-});
-
-let VariationRegionList = new r.Struct({
-  axisCount: r.uint16,
-  regionCount: r.uint16,
-  variationRegions: new r.Array(new r.Array(RegionAxisCoordinates, 'axisCount'), 'regionCount')
-});
-
-let DeltaSet = new r.Struct({
-  shortDeltas: new r.Array(r.int16, t => t.parent.shortDeltaCount),
-  regionDeltas: new r.Array(r.int8, t => t.parent.regionIndexCount - t.parent.shortDeltaCount),
-  deltas: t => t.shortDeltas.concat(t.regionDeltas)
-});
-
-let ItemVariationData = new r.Struct({
-  itemCount: r.uint16,
-  shortDeltaCount: r.uint16,
-  regionIndexCount: r.uint16,
-  regionIndexes: new r.Array(r.uint16, 'regionIndexCount'),
-  deltaSets: new r.Array(DeltaSet, 'itemCount')
-});
-
-let ItemVariationStore = new r.Struct({
-  format: r.uint16,
-  variationRegionList: new r.Pointer(r.uint32, VariationRegionList),
-  variationDataCount: r.uint16,
-  itemVariationData: new r.Array(new r.Pointer(r.uint32, ItemVariationData), 'variationDataCount')
-});
+import {ItemVariationStore} from './variations';
 
 // TODO: add this to restructure
 class VariableSizeNumber {
