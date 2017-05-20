696da60dce867e5c0fe71469ee17c9c2c61f533b
Devon Govett <devongovett@gmail.com>
2017-05-20 16:19:27 -0700

    [indic] Add indic position to trie

--- src/opentype/shapers/IndicShaper.js
+++ src/opentype/shapers/IndicShaper.js
@@ -8,6 +8,42 @@ const {categories} = indicData;
 const trie = new UnicodeTrie(require('fs').readFileSync(__dirname + '/indic.trie'));
 const stateMachine = new StateMachine(indicData);
 
+// Visual positions in a syllable from left to right.
+export const POSITIONS = {
+  Start: 0,
+
+  Ra_To_Become_Reph: 1,
+  Pre_M: 2,
+  Pre_C: 3,
+
+  Base_C: 4,
+  After_Main: 5,
+
+  Above_C: 6,
+
+  Before_Sub: 7,
+  Below_C: 8,
+  After_Sub: 9,
+
+  Before_Post: 10,
+  Post_C: 11,
+  After_Post: 12,
+
+  Final_C: 13,
+  SMVD: 14,
+
+  End: 15
+};
+
+export const IS_CONSONANT = {
+  C: true,
+  Ra: true,
+  CM: true,
+  V: true,
+  Placeholder: true,
+  Dotted_Circle: true
+};
+
 export default class IndicShaper extends DefaultShaper {
   static zeroMarkWidths = 'NONE';
   static planFeatures(plan) {
--- src/opentype/shapers/gen-indic.js
+++ src/opentype/shapers/gen-indic.js
@@ -2,6 +2,7 @@ import codepoints from 'codepoints';
 import fs from 'fs';
 import UnicodeTrieBuilder from 'unicode-trie/builder';
 import compile from 'dfa/compile';
+import {POSITIONS, IS_CONSONANT} from './IndicShaper';
 
 const CATEGORIES = {
   Avagraha: 'Symbol',
@@ -72,9 +73,7 @@ const OVERRIDES = {
   0x17C6: 'N',
   0x2010: 'Placeholder',
   0x2011: 'Placeholder',
-  0x25CC: 'DOTTEDCIRCLE',
-
-
+  0x25CC: 'Dotted_Circle',
 
   // Ra
   0x0930: 'Ra', /* Devanagari */
@@ -91,27 +90,122 @@ const OVERRIDES = {
   0x179A: 'Ra', /* Khmer */		/* No Reph, Visual Repha */
 };
 
+const POSITION_MAP = {
+  Left: 'Pre_C',
+  Top: 'Above_C',
+  Bottom: 'Below_C',
+  Right: 'Post_C',
+
+  // These should resolve to the position of the last part of the split sequence.
+  Bottom_And_Right: 'Post_C',
+  Left_And_Right: 'Post_C',
+  Top_And_Bottom: 'Below_C',
+  Top_And_Bottom_And_Right: 'Post_C',
+  Top_And_Left: 'Above_C',
+  Top_And_Left_And_Right: 'Post_C',
+  Top_And_Right: 'Post_C',
+
+  Overstruck: 'After_Main',
+  Visual_Order_Left: 'Pre_M'
+};
+
+function matraPosition(c, pos) {
+  switch (pos) {
+    case 'Pre_C':
+      return 'Pre_M';
+
+    case 'Post_C':
+      switch (c.block) {
+        case 'Devanagari': return 'After_Sub';
+        case 'Bengali':    return 'After_Post';
+        case 'Gurmukhi':   return 'After_Post';
+        case 'Gujarati':   return 'After_Post';
+        case 'Oriya':      return 'After_Post';
+        case 'Tamil':      return 'After_Post';
+        case 'Telugu':     return c.code <= 0x0C42 ? 'Before_Sub' : 'After_Sub';
+        case 'Kannada':    return c.code < 0x0CC3 || c.code > 0xCD6 ? 'Before_Sub' : 'After_Sub';
+        case 'Malayalam':  return 'After_Post';
+        case 'Sinhala':    return 'After_Sub';
+        case 'Khmer':      return 'After_Post';
+        default:           return 'After_Sub';
+      }
+
+    case 'Above_C':
+      switch (c.block) {
+        case 'Devanagari': return 'After_Sub';
+        case 'Gurmukhi':   return 'After_Post'; // Deviate from spec
+        case 'Gujarati':   return 'After_Sub';
+        case 'Oriya':      return 'After_Main';
+        case 'Tamil':      return 'After_Sub';
+        case 'Telugu':     return 'Before_Sub';
+        case 'Kannada':    return 'Before_Sub';
+        case 'Sinhala':    return 'After_Sub';
+        case 'Khmer':      return 'After_Post';
+        default:           return 'After_Sub';
+      }
+
+    case 'Below_C':
+      switch (c.block) {
+        case 'Devanagari': return 'After_Sub';
+        case 'Bengali':    return 'After_Sub';
+        case 'Gurmukhi':   return 'After_Post';
+        case 'Gujarati':   return 'After_Post';
+        case 'Oriya':      return 'After_Sub';
+        case 'Tamil':      return 'After_Post';
+        case 'Telugu':     return 'Before_Sub';
+        case 'Kannada':    return 'Before_Sub';
+        case 'Malayalam':  return 'After_Post';
+        case 'Sinhala':    return 'After_Sub';
+        case 'Khmer':      return 'After_Post';
+        default:           return 'After_Sub';
+      }
+
+    default:
+      return pos;
+  }
+}
+
+function getPosition(codepoint, category) {
+  let position = POSITION_MAP[codepoint.indicPositionalCategory] || 'End';
+
+  if (IS_CONSONANT[category]) {
+    position = 'Base_C';
+  } else if (category === 'M') {
+    position = matraPosition(codepoint, position);
+  } else if (category === 'SM' || category === 'VD' || category === 'A' || category === 'Symbol') {
+    position = 'SMVD';
+  }
+
+  // Oriya Bindu is Before_Sub in the spec.
+  if (codepoint.code === 0x0B01) {
+    position = 'Before_Sub';
+  }
+
+  return POSITIONS[position];
+}
+
 let trie = new UnicodeTrieBuilder;
-let symbols = {};
-let numSymbols = 0;
+let categories = {};
+let numCategories = 0;
 let decompositions = {};
 for (let i = 0; i < codepoints.length; i++) {
   let codepoint = codepoints[i];
   if (codepoint) {
     let category = OVERRIDES[codepoint.code] || CATEGORIES[codepoint.indicSyllabicCategory] || 'X';
-    if (!(category in symbols)) {
-      symbols[category] = numSymbols++;
+    let position = getPosition(codepoint, category);
+    if (!(category in categories)) {
+      categories[category] = numCategories++;
     }
 
-    trie.set(codepoint.code, symbols[category]);
+    trie.set(codepoint.code, (categories[category] << 8) | position);
   }
 }
 
 fs.writeFileSync(__dirname + '/indic.trie', trie.toBuffer());
 
-let stateMachine = compile(fs.readFileSync(__dirname + '/indic.machine', 'utf8'), symbols);
+let stateMachine = compile(fs.readFileSync(__dirname + '/indic.machine', 'utf8'), categories);
 let json = Object.assign({
-  categories: Object.keys(symbols)
+  categories: Object.keys(categories)
 }, stateMachine);
 
 fs.writeFileSync(__dirname + '/indic.json', JSON.stringify(json));
