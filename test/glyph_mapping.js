foliojs/fontkit

commit 2116c8f629f65965c4ab67192320d40f28af5224

Allow user to override shaper features
Also pass back the script, language, and features that were selected as part of the GlyphRun.

src/aat/AATLayoutEngine.js
@@ -9,16 +9,14 @@ export default class AATLayoutEngine {
     this.fallbackPosition = false;
   }
 
-  substitute(glyphs, features, script, language) {
+  substitute(glyphRun) {
     // AAT expects the glyphs to be in visual order prior to morx processing,
     // so reverse the glyphs if the script is right-to-left.
-    let isRTL = Script.direction(script) === 'rtl';
-    if (isRTL) {
+    if (glyphRun.direction === 'rtl') {
       glyphs.reverse();
     }
 
-    this.morxProcessor.process(glyphs, AATFeatureMap.mapOTToAAT(features));
-    return glyphs;
+    this.morxProcessor.process(glyphRun.glyphs, AATFeatureMap.mapOTToAAT(glyphRun.features));
   }
 
   getAvailableFeatures(script, language) {

src/aat/AATFeatureMap.js
@@ -478,14 +478,14 @@ for (let ot in OTMapping) {
 // in the form of {featureType:{featureSetting:true}}
 export function mapOTToAAT(features) {
   let res = {};
-  for (let k = 0; k < features.length; k++) {
+  for (let k in features) {
     let r;
-    if (r = OTMapping[features[k]]) {
+    if (r = OTMapping[k]) {
       if (res[r[0]] == null) {
         res[r[0]] = {};
       }
 
-      res[r[0]][r[1]] = true;
+      res[r[0]][r[1]] = features[k];
     }
   }
 

src/layout/GlyphRun.js
@@ -1,11 +1,12 @@
 import BBox from '../glyph/BBox';
+import * as Script from '../layout/Script';
 
 /**
  * Represents a run of Glyph and GlyphPosition objects.
  * Returned by the font layout method.
  */
 export default class GlyphRun {
-  constructor(glyphs, positions) {
+  constructor(glyphs, features, script, language) {
     /**
      * An array of Glyph objects in the run
      * @type {Glyph[]}
@@ -16,7 +17,42 @@ export default class GlyphRun {
      * An array of GlyphPosition objects for each glyph in the run
      * @type {GlyphPosition[]}
      */
-    this.positions = positions;
+    this.positions = null;
+
+    /**
+     * The script that was used for shaping. This was either passed in or detected automatically.
+     * @type {string}
+     */
+    this.script = script;
+
+    /**
+     * The language used for shaping, as passed in. If `null`, the default language for the
+     * script was used.
+     * @type {string}
+     */
+    this.language = language || null;
+
+    /**
+     * The directionality of the script (either ltr or rtl).
+     * @type {string}
+     */
+    this.direction = Script.direction(script);
+
+    /**
+     * The features applied during shaping. This is a combination of user
+     * specified features and features chosen by the shaper.
+     * @type {object}
+     */
+    this.features = {};
+
+    // Convert features to an object
+    if (Array.isArray(features)) {
+      for (let tag of features) {
+        this.features[tag] = true;
+      }
+    } else if (typeof features === 'object') {
+      this.features = features;
+    }
   }
 
   /**

src/layout/LayoutEngine.js
@@ -23,7 +23,7 @@ export default class LayoutEngine {
     }
   }
 
-  layout(string, features = [], script, language) {
+  layout(string, features, script, language) {
     // Make the features parameter optional
     if (typeof features === 'string') {
       script = features;
@@ -53,45 +53,45 @@ export default class LayoutEngine {
       var glyphs = string;
     }
 
+    let glyphRun = new GlyphRun(glyphs, features, script, language);
+
     // Return early if there are no glyphs
     if (glyphs.length === 0) {
-      return new GlyphRun(glyphs, []);
+      return glyphRun;
     }
 
     // Setup the advanced layout engine
     if (this.engine && this.engine.setup) {
-      this.engine.setup(glyphs, features, script, language);
+      this.engine.setup(glyphRun);
     }
 
     // Substitute and position the glyphs
-    glyphs = this.substitute(glyphs, features, script, language);
-    let positions = this.position(glyphs, features, script, language);
+    this.substitute(glyphRun);
+    this.position(glyphRun);
 
     // Let the layout engine clean up any state it might have
     if (this.engine && this.engine.cleanup) {
       this.engine.cleanup();
     }
 
-    return new GlyphRun(glyphs, positions);
+    return glyphRun;
   }
 
-  substitute(glyphs, features, script, language) {
+  substitute(glyphRun) {
     // Call the advanced layout engine to make substitutions
     if (this.engine && this.engine.substitute) {
-      glyphs = this.engine.substitute(glyphs, features, script, language);
+      this.engine.substitute(glyphRun);
     }
-
-    return glyphs;
   }
 
-  position(glyphs, features, script, language) {
+  position(glyphRun) {
     // Get initial glyph positions
-    let positions = glyphs.map(glyph => new GlyphPosition(glyph.advanceWidth));
+    glyphRun.positions = glyphRun.glyphs.map(glyph => new GlyphPosition(glyph.advanceWidth));
     let positioned = null;
 
     // Call the advanced layout engine. Returns the features applied.
     if (this.engine && this.engine.position) {
-      positioned = this.engine.position(glyphs, positions, features, script, language);
+      positioned = this.engine.position(glyphRun);
     }
 
     // if there is no GPOS table, use unicode properties to position marks.
@@ -100,19 +100,18 @@ export default class LayoutEngine {
         this.unicodeLayoutEngine = new UnicodeLayoutEngine(this.font);
       }
 
-      this.unicodeLayoutEngine.positionGlyphs(glyphs, positions);
+      this.unicodeLayoutEngine.positionGlyphs(glyphRun.glyphs, glyphRun.positions);
     }
 
     // if kerning is not supported by GPOS, do kerning with the TrueType/AAT kern table
-    if ((!positioned || !positioned.kern) && this.font.kern) {
+    if ((!positioned || !positioned.kern) && glyphRun.features.kern !== false && this.font.kern) {
       if (!this.kernProcessor) {
         this.kernProcessor = new KernProcessor(this.font);
       }
 
-      this.kernProcessor.process(glyphs, positions);
+      this.kernProcessor.process(glyphRun.glyphs, glyphRun.positions);
+      glyphRun.features.kern = true;
     }
-
-    return positions;
   }
 
   getAvailableFeatures(script, language) {

src/opentype/OTLayoutEngine.js
@@ -22,46 +22,49 @@ export default class OTLayoutEngine {
     }
   }
 
-  setup(glyphs, features, script, language) {
+  setup(glyphRun) {
     // Map glyphs to GlyphInfo objects so data can be passed between
     // GSUB and GPOS without mutating the real (shared) Glyph objects.
-    this.glyphInfos = glyphs.map(glyph => new GlyphInfo(this.font, glyph.id, [...glyph.codePoints]));
+    this.glyphInfos = glyphRun.glyphs.map(glyph => new GlyphInfo(this.font, glyph.id, [...glyph.codePoints]));
 
     // Choose a shaper based on the script, and setup a shaping plan.
     // This determines which features to apply to which glyphs.
-    this.shaper = Shapers.choose(script);
-    this.plan = new ShapingPlan(this.font, script, language);
-    return this.shaper.plan(this.plan, this.glyphInfos, features);
+    this.shaper = Shapers.choose(glyphRun.script);
+    this.plan = new ShapingPlan(this.font, glyphRun.script, glyphRun.language);
+    this.shaper.plan(this.plan, this.glyphInfos, glyphRun.features);
+
+    // Assign chosen features to output glyph run
+    for (let key in this.plan.allFeatures) {
+      glyphRun.features[key] = true;
+    }
   }
 
-  substitute(glyphs) {
+  substitute(glyphRun) {
     if (this.GSUBProcessor) {
       this.plan.process(this.GSUBProcessor, this.glyphInfos);
 
       // Map glyph infos back to normal Glyph objects
-      glyphs = this.glyphInfos.map(glyphInfo => this.font.getGlyph(glyphInfo.id, glyphInfo.codePoints));
+      glyphRun.glyphs = this.glyphInfos.map(glyphInfo => this.font.getGlyph(glyphInfo.id, glyphInfo.codePoints));
     }
-
-    return glyphs;
   }
 
-  position(glyphs, positions) {
+  position(glyphRun) {
     if (this.shaper.zeroMarkWidths === 'BEFORE_GPOS') {
-      this.zeroMarkAdvances(positions);
+      this.zeroMarkAdvances(glyphRun.positions);
     }
 
     if (this.GPOSProcessor) {
-      this.plan.process(this.GPOSProcessor, this.glyphInfos, positions);
+      this.plan.process(this.GPOSProcessor, this.glyphInfos, glyphRun.positions);
     }
 
     if (this.shaper.zeroMarkWidths === 'AFTER_GPOS') {
-      this.zeroMarkAdvances(positions);
+      this.zeroMarkAdvances(glyphRun.positions);
     }
 
     // Reverse the glyphs and positions if the script is right-to-left
-    if (this.plan.direction === 'rtl') {
-      glyphs.reverse();
-      positions.reverse();
+    if (glyphRun.direction === 'rtl') {
+      glyphRun.glyphs.reverse();
+      glyphRun.positions.reverse();
     }
 
     return this.GPOSProcessor && this.GPOSProcessor.features;

src/opentype/ShapingPlan.js
@@ -24,22 +24,18 @@ export default class ShapingPlan {
    * Adds the given features to the last stage.
    * Ignores features that have already been applied.
    */
-  _addFeatures(features) {
-    let stage = this.stages[this.stages.length - 1];
+  _addFeatures(features, global) {
+    let stageIndex = this.stages.length - 1;
+    let stage = this.stages[stageIndex];
     for (let feature of features) {
-      if (!this.allFeatures[feature]) {
+      if (this.allFeatures[feature] == null) {
         stage.push(feature);
-        this.allFeatures[feature] = true;
+        this.allFeatures[feature] = stageIndex;
       }
-    }
-  }
 
-  /**
-   * Adds the given features to the global list
-   */
-  _addGlobal(features) {
-    for (let feature of features) {
-      this.globalFeatures[feature] = true;
+      if (global) {
+        this.globalFeatures[feature] = true;
+      }
     }
   }
 
@@ -56,16 +52,10 @@ export default class ShapingPlan {
     }
 
     if (Array.isArray(arg)) {
-      this._addFeatures(arg);
-      if (global) {
-        this._addGlobal(arg);
-      }
+      this._addFeatures(arg, global);
     } else if (typeof arg === 'object') {
-      let features = (arg.global || []).concat(arg.local || []);
-      this._addFeatures(features);
-      if (arg.global) {
-        this._addGlobal(arg.global);
-      }
+      this._addFeatures(arg.global || [], true);
+      this._addFeatures(arg.local || [], false);
     } else {
       throw new Error("Unsupported argument to ShapingPlan#add");
     }
@@ -80,6 +70,23 @@ export default class ShapingPlan {
     } else {
       this.stages.push([]);
       this.add(arg, global);
+    }
+  }
+
+  setFeatureOverrides(features) {
+    if (Array.isArray(features)) {
+      this.add(features);
+    } else if (typeof features === 'object') {
+      for (let tag in features) {
+        if (features[tag]) {
+          this.add(tag);
+        } else if (this.allFeatures[tag] != null) {
+          let stage = this.stages[this.allFeatures[tag]];
+          stage.splice(stage.indexOf(tag), 1);
+          delete this.allFeatures[tag];
+          delete this.globalFeatures[tag];
+        }
+      }
     }
   }
 

src/opentype/shapers/DefaultShaper.js
@@ -37,7 +37,8 @@ export default class DefaultShaper {
   }
 
   static planPostprocessing(plan, userFeatures) {
-    plan.add([...COMMON_FEATURES, ...HORIZONTAL_FEATURES, ...userFeatures]);
+    plan.add([...COMMON_FEATURES, ...HORIZONTAL_FEATURES]);
+    plan.setFeatureOverrides(userFeatures);
   }
 
   static assignFeatures(plan, glyphs) {

commit ff3ec34cc4c865051b6a1b8e138d03e08e31905c

Fix RTL handling with AAT fonts
Regression from 2116c8f629f65965c4ab67192320d40f28af5224.

src/aat/AATLayoutEngine.js
@@ -13,7 +13,7 @@ export default class AATLayoutEngine {
     // AAT expects the glyphs to be in visual order prior to morx processing,
     // so reverse the glyphs if the script is right-to-left.
     if (glyphRun.direction === 'rtl') {
-      glyphs.reverse();
+      glyphRun.glyphs.reverse();
     }
 
     this.morxProcessor.process(glyphRun.glyphs, AATFeatureMap.mapOTToAAT(glyphRun.features));

test/glyph_mapping.js
@@ -91,6 +91,13 @@ describe('character to glyph mapping', function() {
       return assert.deepEqual(glyphs.map(g => g.codePoints), [[102, 102, 105], [32], [49, 8260, 50]]);
     });
 
+    it('should handle rtl direction', function() {
+      let {glyphs} = font.layout('ffi', [], null, null, "rtl");
+      assert.equal(glyphs.length, 3);
+      assert.deepEqual(glyphs.map(g => g.id), [ 76, 73, 73 ]);
+      return assert.deepEqual(glyphs.map(g => g.codePoints), [[105], [102], [102]]);
+    });
+
     it('should apply indic reordering features', function() {
       let f = fontkit.openSync(__dirname + '/data/Khmer/Khmer.ttf');
       let {glyphs} = f.layout('ខ្ញុំអាចញ៉ាំកញ្ចក់បាន ដោយគ្មានបញ្ហា');