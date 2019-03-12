61a27911bd7b6c14d297eff8a0306063ea21990a
Luiz Américo <luizamericop@gmail.com>
2019-03-11 21:36:32 -0300

    Add es5 (ie11 compatible) build

--- rollup.config.js
+++ rollup.config.js
@@ -2,52 +2,68 @@ import babel from 'rollup-plugin-babel';
 import localResolve from 'rollup-plugin-local-resolve';
 import json from 'rollup-plugin-json';
 
-export default {  
-  output: {
-    format: 'cjs',
-    sourcemap: true
-  },
-  external: [
-    'restructure',
-    'tiny-inflate',
-    'brotli/decompress',
-    'unicode-properties',
-    'clone',
-    'deep-equal',
-    'unicode-trie',
-    'dfa',
-    'restructure/src/utils'
-  ],
-  plugins: [
-    localResolve(),
-    json(),
-    babel({
-      presets: [
-        [
-          '@babel/preset-env', 
-          {
-            modules: false,
-            targets: {
-              node: '8.11',
-              browsers: [
-                'Firefox 57',
-                'Chrome 60',
-                'iOS 10',
-                'Safari 10'
-              ]
+function createConfig(filename, browserlist, suffix = '') {
+  return {
+    input: `src/${filename}`,
+    output: {
+      file: `${filename}${suffix}.js`,
+      format: 'cjs',
+      sourcemap: true
+    },
+    external: [
+      'restructure',
+      'tiny-inflate',
+      'brotli/decompress',
+      'unicode-properties',
+      'clone',
+      'deep-equal',
+      'unicode-trie',
+      'dfa',
+      'restructure/src/utils'
+    ],
+    plugins: [
+      localResolve(),
+      json(),
+      babel({
+        presets: [
+          [
+            '@babel/preset-env', 
+            {
+              modules: false,
+              targets: {
+                browsers: browserlist
+              }
             }
-          }
-        ]
-      ],
-      plugins: [
-        [
-          '@babel/plugin-proposal-decorators',
-          {
-            legacy: true
-          }
+          ]
         ],
-        '@babel/plugin-proposal-class-properties'
-      ]
-    })
-  ]
-};
+        plugins: [
+          [
+            '@babel/plugin-proposal-decorators',
+            {
+              legacy: true
+            }
+          ],
+          '@babel/plugin-proposal-class-properties'
+        ]
+      })
+    ]
+  }
+}
+
+const modernBrowsers = [
+  'Firefox 57',
+  'Chrome 60',
+  'iOS 10',
+  'Safari 10'
+];
+
+const legacyBrowsers = [
+  'ie 11'
+];
+
+export default [
+  createConfig('base', modernBrowsers),
+  createConfig('index', modernBrowsers),
+  createConfig('base', legacyBrowsers, '.es5'),
+  createConfig('index', legacyBrowsers, '.es5')
+];
