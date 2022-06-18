540b8ec02fe77fa0dda68e768ea209c65b24bf6a
toastal <561087+toastal@users.noreply.github.com>
2022-06-18 22:53:22 +0000

    Faster, smaller `isEqual` (#279)

--- src/cff/CFFDict.js
+++ src/cff/CFFDict.js
@@ -1,4 +1,4 @@
-import isEqual from 'deep-equal';
+import isEqual from 'fast-deep-equal';
 import * as r from 'restructure';
 import CFFOperand from './CFFOperand';
 import { PropertyDescriptor } from 'restructure';
