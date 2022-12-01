388e6ae127cbefcb89eb5e03af41b2c327bac3d2
Simon Cozens <simon@simon-cozens.org>
2022-12-01 21:43:59 +0000

    Move DeltaSetIndexMap to variations

--- src/tables/HVAR.js
+++ src/tables/HVAR.js
@@ -1,38 +1,6 @@
 import * as r from 'restructure';
 import { resolveLength } from 'restructure';
-import { ItemVariationStore } from './variations';
-
-// TODO: add this to restructure
-class VariableSizeNumber {
-  constructor(size) {
-    this._size = size;
-  }
-
-  decode(stream, parent) {
-    switch (this.size(0, parent)) {
-      case 1: return stream.readUInt8();
-      case 2: return stream.readUInt16BE();
-      case 3: return stream.readUInt24BE();
-      case 4: return stream.readUInt32BE();
-    }
-  }
-
-  size(val, parent) {
-    return resolveLength(this._size, null, parent);
-  }
-}
-
-let MapDataEntry = new r.Struct({
-  entry: new VariableSizeNumber(t => ((t.parent.entryFormat & 0x0030) >> 4) + 1),
-  outerIndex: t => t.entry >> ((t.parent.entryFormat & 0x000F) + 1),
-  innerIndex: t => t.entry & ((1 << ((t.parent.entryFormat & 0x000F) + 1)) - 1)
-});
-
-let DeltaSetIndexMap = new r.Struct({
-  entryFormat: r.uint16,
-  mapCount: r.uint16,
-  mapData: new r.Array(MapDataEntry, 'mapCount')
-});
+import { ItemVariationStore, DeltaSetIndexMap } from './variations';
 
 export default new r.Struct({
   majorVersion: r.uint16,
--- src/tables/variations.js
+++ src/tables/variations.js
@@ -39,6 +39,43 @@ export let ItemVariationStore = new r.Struct({
   itemVariationData: new r.Array(new r.Pointer(r.uint32, ItemVariationData), 'variationDataCount')
 });
 
+/***********************
+ * Delta Set Index Map *
+ ***********************/
+
+// TODO: add this to restructure
+class VariableSizeNumber {
+  constructor(size) {
+    this._size = size;
+  }
+
+  decode(stream, parent) {
+    switch (this.size(0, parent)) {
+      case 1: return stream.readUInt8();
+      case 2: return stream.readUInt16BE();
+      case 3: return stream.readUInt24BE();
+      case 4: return stream.readUInt32BE();
+    }
+  }
+
+  size(val, parent) {
+    return r.resolveLength(this._size, null, parent);
+  }
+}
+
+let MapDataEntry = new r.Struct({
+  entry: new VariableSizeNumber(t => ((t.parent.entryFormat & 0x0030) >> 4) + 1),
+  outerIndex: t => t.entry >> ((t.parent.entryFormat & 0x000F) + 1),
+  innerIndex: t => t.entry & ((1 << ((t.parent.entryFormat & 0x000F) + 1)) - 1)
+});
+
+export let DeltaSetIndexMap = new r.Struct({
+  entryFormat: r.uint16,
+  mapCount: r.uint16,
+  mapData: new r.Array(MapDataEntry, 'mapCount')
+});
+
+
 /**********************
  * Feature Variations *
  **********************/
